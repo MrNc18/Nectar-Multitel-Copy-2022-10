3f8524b64293c5cd0fbdda37dc38a1c3178082f8
akash.yadav@nectarinfotel.com <akashkumaryadav.codesmasher@gmail.com>
2022-03-28 15:38:28 +0530

    Initial commit

--- src/components/atoms/Profile.js
+++ src/components/atoms/Profile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Col, Container, Row, Card, Nav } from "react-bootstrap";
+import { Button, Col, Container, Row, Card, Nav, CardGroup } from "react-bootstrap";
 
 function Profile() {
   return (
@@ -63,7 +63,50 @@ function Profile() {
               </Col>
               <Col md={12}>
               <Card.Text>Account Overview</Card.Text>
-              
+              <CardGroup>
+              <Card>
+                <Card.Body style={{textAlign:"center"}}>
+                  <Card.Text style={{ display: "inline-block", marginRight:"30px", fontWeight:"bold"}}>Bills & Payments</Card.Text>
+                  <Card.Link href="#" ><u>View</u></Card.Link>
+                  <Card.Text>
+                    No Bills are pending
+                </Card.Text>
+                </Card.Body>
+              </Card>
+              <Card>
+                <Card.Body style={{textAlign:"center"}}>
+                  <Card.Text style={{ display: "inline-block", marginRight:"5px", fontWeight:"bold"}}>Balanace & Recharge</Card.Text>
+                  <Card.Link href="#" ><u>View</u></Card.Link>
+                  <Card.Text>
+                    <p style={{color:"#0076B5"}}>$20.00 Balance in your</p>
+                    <h5>Mobile</h5>
+                </Card.Text>
+                <Button style={{backgroundColor:"#0076B5", background:"#0076B5"}}>Process To Pay</Button>{' '}
+                </Card.Body>
+              </Card>
+              <Card>
+                <Card.Body style={{textAlign:"center"}}>
+                  <Card.Text style={{ display: "inline-block", marginRight:"30px", fontWeight:"bold"}}>Robert Plaska</Card.Text>
+                  <Card.Link href="#" ><u>View</u></Card.Link>
+                  <Card.Text>
+                  <p><i class="fa-solid fa-mobile-screen-button"></i> 85426-57894</p>
+                  <p style={{color:"#0076B5"}}>$20.00 Balance in your</p>
+                    <h5>Mobile</h5>
+                </Card.Text>
+                </Card.Body>
+              </Card>
+              <Card>
+                <Card.Body style={{textAlign:"center"}}>
+                  <Card.Text style={{ display: "inline-block", marginRight:"30px", fontWeight:"bold"}}>Broadband</Card.Text>
+                  <Card.Link href="#" ><u>View</u></Card.Link>
+                  <p><i class="fa-solid fa-globe"></i> 85426-57894</p>
+                  <Card.Text>
+                  <p style={{color:"#0076B5"}}>Available For</p>
+                    <h5>25 Days</h5>
+                </Card.Text>
+                </Card.Body>
+              </Card>
+              </CardGroup>
               </Col>
             </Card.Body>
           </Card>
