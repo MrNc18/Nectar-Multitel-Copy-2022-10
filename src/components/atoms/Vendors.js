55d7669af5ed19b1003c12b771f544e146a2fbe0
akash.yadav@nectarinfotel.com <akashkumaryadav.codesmasher@gmail.com>
2022-04-08 14:11:49 +0530

    Initial commit

--- src/components/atoms/Vendors.js
+++ src/components/atoms/Vendors.js
@@ -1,38 +1,101 @@
-import React from 'react';
-import { Container, Col, Row } from "react-bootstrap";
-import VendorsTable from './VendorsTable';
+import React, {useState} from "react";
+import { Container, Col, Row, Table } from "react-bootstrap";
+import ReactPaginate from "react-paginate";
 
 function Vendors() {
-    const theadData = ["Name", "Email", "Date"];
-
-  const tbodyData = [
+  const vendors = [
     {
-      id: "1",
-      items: ["John", "john@email.com", "01/01/2021"],
+      name: "Mark",
+      phone: "245-568-5698",
+      email: "mark@gmail.com",
+      address: "1st Block 1st Cross, Rammurthy nagar",
     },
     {
-      id: "2",
-      items: ["Sally", "sally@email.com", "12/24/2020"],
+      name: "Mark",
+      phone: "245-568-5698",
+      email: "mark@gmail.com",
+      address: "1st Block 1st Cross, Rammurthy nagar",
     },
     {
-      id: "3",
-      items: ["Maria", "maria@email.com", "12/01/2020"],
+      name: "Mark",
+      phone: "245-568-5698",
+      email: "mark@gmail.com",
+      address: "1st Block 1st Cross, Rammurthy nagar",
+    },
+    {
+      name: "Mark",
+      phone: "245-568-5698",
+      email: "mark@gmail.com",
+      address: "1st Block 1st Cross, Rammurthy nagar",
+    },
+    {
+      name: "Mark",
+      phone: "245-568-5698",
+      email: "mark@gmail.com",
+      address: "1st Block 1st Cross, Rammurthy nagar",
     },
   ];
+
+  //pagination
+  const [pageNumber, setPageNumber] = useState(0);
+  const usersPerPage = 3;
+  const pagesVisited = pageNumber * usersPerPage;
+  const pageCount = Math.ceil(vendors.length / usersPerPage);
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
   return (
     <Container>
-        <Row>
-            <Col md={12}>
-            {tbodyData.map((service) => (
-              <VendorsTable
-                key={service.id}
-                items={service.items}
+      <Row>
+        <Col md={12}>
+          <Table responsive="md" className="mt-4 mb-4">
+            <thead style={{ backgroundColor: "#0076B5", color: "white" }}>
+              <tr>
+                <th>Vendor Name</th>
+                <th>Vendor Phone</th>
+                <th>Vendor Email</th>
+                <th>Vendor Address</th>
+              </tr>
+            </thead>
+            <tbody>
+              {vendors.slice(pagesVisited, pagesVisited + usersPerPage).map((item, i) => (
+                <tr key={i}>
+                  <td>{item.name}</td>
+                  <td>{item.phone}</td>
+                  <td>{item.email}</td>
+                  <td>{item.address}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+          <Row>
+            <div className="col-md-7"></div>
+            <div
+              className="col-md-5"
+              style={{
+                display: "inherit",
+                marginBottom: "20px",
+                marginTop: "20px",
+              }}
+            >
+              <ReactPaginate
+                previousLabel={<i class="fa-solid fa-arrow-left fa-lg"></i>}
+                nextLabel={<i class="fa-solid fa-arrow-right fa-lg"></i>}
+                pageCount={pageCount}
+                onPageChange={changePage}
+                containerClassName={"paginationBttns"}
+                previousLinkClassName={"previousBttn"}
+                nextLinkClassName={"nextBttn"}
+                disabledClassName={"paginationDisabled"}
+                activeClassName={"paginationActive"}
               />
-            ))}
-            </Col>
-        </Row>
+            </div>
+          </Row>
+        </Col>
+      </Row>
     </Container>
-  )
+  );
 }
 
-export default Vendors
+export default Vendors;
