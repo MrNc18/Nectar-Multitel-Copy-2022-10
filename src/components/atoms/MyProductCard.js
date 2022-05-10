2ca714e0d5a6206aeaa6a82fb7e3260bcf66817f
MrNc18 <karrinagasai12@gmail.com>
2022-05-10 07:03:30 +0530

    Invoice chnages and profile

--- src/components/atoms/MyProductCard.js
+++ src/components/atoms/MyProductCard.js
@@ -1,145 +1,138 @@
 import React, { useState, useEffect } from "react";
 import { Button, Col, Container, Row, Card, CardGroup } from "react-bootstrap";
-import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { getmyorders } from "../../services/category";
-import { AUTH_TOKEN, deleteCookie, getCookie } from "../../utils/cookie";
-import { getUserDetailsByToken } from "../../services/authentication";
-import { Table } from "react-bootstrap";
+import { formatAmount } from "../../utils/AmountFormatter";
 import { showAlert } from "../../utils/showAlert";
+import { imageUrl } from "../../services/category";
 
 function MyProductCard() {
+  const [orderList, setOrderList] = useState("");
+  const userId = sessionStorage.getItem("userId");
+  const navigate = useNavigate();
 
+  const handleAllOrders = async () => {
+    const data = {
+      userId: userId,
+    };
+    try {
+      const resp = await getmyorders(data);
+      let Filtered = [];
+      for (let i = 0; i < resp.data.data.length; i++) {
+        if (resp.data.data[i].transaction_id != null) {
+          Filtered.push(resp.data.data[i]);
+        }
+      }
+      console.log("FFF", Filtered);
+      setOrderList(Filtered);
+      console.log("resp", resp.data.data);
+    } catch (error) {
+      console.log("error", error);
+      showAlert("something went Wrong", "error");
+    }
+  };
+  useEffect(() => {
+    handleAllOrders();
+  }, []);
 
-  // const { state } = useLocation();
-  // const isAuthenticated = getCookie(AUTH_TOKEN);
-  // const [orderList, setOrderList] = useState("");
-  // const [userData, setUserData] = useState("");
- 
-  // const userId = sessionStorage.getItem("userId")
-  // console.log("state",userId)
+  const orderDetails = (order_detail) => {
+    const data = JSON.parse(order_detail);
+    console.log("murthyfgfgh", data);
+    return (
+      data &&
+      data.length >= 0 &&
+      data[0].products &&
+      data[0].products.map((item) => {
+        console.log("murthy", item);
+        return (
+          <div>
+            <p>{item.product_name}</p>
+          </div>
+        );
+      })
+    );
+  };
 
+  const image = (order_detail) => {
+    const data = JSON.parse(order_detail);
+    console.log("murthyfgfgh", data);
+    return (
+      data &&
+      data.length >= 0 &&
+      data[0].products &&
+      data[0].products.map((item) => {
+        console.log("murthy", item.product_image);
+        return (
+          <div>
+            <p>{imageUrl(item.product_image)}</p>
+          </div>
+        );
+      })
+    );
+  };
+  const price = (order_detail) => {
+    const data = JSON.parse(order_detail);
+    return (
+      data &&
+      data.length >= 0 &&
+      data[0].products &&
+      data[0].products.map((item) => {
+        console.log("ppppp", item.product_price);
+        return (
+          <div>
+            <p>{formatAmount(item.product_price)}</p>
+          </div>
+        );
+      })
+    );
+  };
 
-
-
-  // const handleAllOrders = async () => {
-  //   console.log("user",userData)
-  //   const data = {
-  //     userId: userId
-  //   };
-  //   try {
-  //     const resp = await getmyorders(data);
-  //     setOrderList(resp && resp.data.data);
-  //     console.log("resp", resp);
-  //   } catch (error) {
-  //     console.log("error", error);
-  //     showAlert("something went Wrong", "error");
-  //   }
-  // };
-  // useEffect(() => {
-  //   handleAllOrders();
-  // }, []);
   return (
     <Container className="mb-4">
-       <Row>
-        <Col md={12}>
-          <h5 style={{ color: "#0076B5" }}>Check the products below</h5>
-          <h5>Broadband(1)</h5>
-          <CardGroup
-            style={{
-              backgroundColor: "#F5F5F5",
-              marginTop: "30px",
-              padding: "15px",
-            }}
-          >
-            <Col md={6}>
-              <Card style={{ background: "none", border: "none" }}>
-                <Card.Body>
-                  <h4 style={{ color: "#0076B5" }}>
-                    Your Unlimited GB+ Talk plan runs at
-                  </h4>
-                  <h4>100 Mbps</h4>
-                  <p style={{ display: "inline-block" }}>
-                    Rs. 958 per bill cycle{" "}
-                  </p>{" "}
-                  <u
-                    style={{
-                      color: "#0076B5",
-                      display: "inline-block",
-                      marginLeft: "20px",
-                    }}
-                  >
-                    <p> Want to Recharge?</p>
-                  </u>
-                  <Button
-                    size="md"
-                    style={{
-                      backgroundColor: "#0076B5",
-                      background: "#0076B5",
-                    }}
-                  >
-                    <i class="fa-solid fa-download mr-1"></i> Download Multitel
-                    Mobile App
-                  </Button>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col md={6}>
-              <Card style={{ background: "none", border: "none", marginTop:"50px" }}>
-                <Card.Body>
-                  <div style={{ display: "inline-block", marginLeft:"30px" }}>
-                    <p>Speed</p>
-                    <h5>100 Mbps</h5>
-                  </div>
-                  <div style={{ display: "inline-block", marginLeft:"30px" }}>
-                    <p>Data</p>
-                    <h5>Data not Available</h5>
-                  </div>
-                  <div style={{ display: "inline-block", marginLeft:"30px" }}>
-                    <p>Talktime</p>
-                    <h5>Unlimited</h5>
-                  </div>
-                </Card.Body>
-              </Card>
-            </Col>
-          </CardGroup>
-        </Col>
-      </Row> 
-      {/* <div id="layoutSidenavContent">
-        <div className="container-fluid">
-          <div class="row d-flex align-items-center justify-content-between">
-            <div class="col-lg-6 col-md-6 text-left">
-              <h3 className="mt-4 mb-4"> All Orders</h3>
-            </div>
-          </div>
-          <Table striped bordered hover size="md" responsive>
-            <thead style={{ backgroundColor: "#0076B5", color: "white" }}>
+      <div className="row">
+        <div className="col-md-12 table-responsive">
+          <table className="book_info_tbl w-100">
+            <tr className="header_row">
+              <td>Product</td>
+              <td className="text-center price_col">Price</td>
+              {/* <td className="text-center qty_col">Status</td> */}
+              <td className="text-center">Subtotal</td>
+            </tr>
+            {orderList ? (
+              orderList.map((item) => (
+                <tr>
+                  {console.log("oder", orderList)}
+                  <td>
+                    <div className="d-flex align-items-center">
+                      <img
+                        className="cart_book_img"
+                        // src={imageUrl(`${image(item.order_detail)}`)}
+                        src={item ? image(item.order_detail) : ""}
+                        style={{ cursor: "pointer" }}
+                      />
+                      <span className="cart_book_name">
+                        {item ? orderDetails(item.order_detail) : ""}
+                      </span>
+                    </div>
+                  </td>
+                  <td className="cart_price text-center">
+                    {item ? price(item.order_detail) : ""}
+                  </td>
+                  <td className="cart_price text-center">
+                    {formatAmount(item.amount)}
+                  </td>
+                  {/* {setProdId(item.id)} */}
+                  {/* <td><a onClick={()=>removeFromBasket(item)} className="remove_from_cart">Remove</a></td> */}
+                </tr>
+              ))
+            ) : (
               <tr>
-                <th>Sr.No.</th>
-                <th>Reference Id</th>
-                <th>Terminal type</th>
-                <th>Transaction Id</th>
-                <th>Product Details</th>
-                <th>Total Price</th>
+                <td colspan="5">No items added to cart.</td>
               </tr>
-            </thead>
-            <tbody>
-              {console.log("oedrs", orderList)}
-              {orderList &&
-                orderList.map((item, i) => (
-                  <tr>
-                    {console.log("hxwjh", orderList)}
-                    <td>{i + 1}</td>
-                    <td>{item ? item.reference_id : ""}</td>
-                    <td>{item ? item.terminal_type : ""}</td>
-                    <td>{item ? item.transaction_id : ""}</td>
-                    <td>{item ? item.order_detail : ""}</td>
-                    <td>{item ? item.amount : ""}</td>
-                  </tr>
-                ))}
-            </tbody>
-          </Table>
+            )}
+          </table>
         </div>
-      </div> */}
+      </div>
     </Container>
   );
 }
