37e62ddd4186643df456429c8a9a350d02241e20
barkha sharma <barkha.sharma55722@gmail.com>
2022-07-13 17:34:06 +0530

    Initial commit

--- src/containers/Cpes.js
+++ src/containers/Cpes.js
@@ -1,37 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ServiceBanner from "../components/atoms/ServiceBanner";
 import LandingPage from "../components/LandingPage";
 import Breadcrumb from "react-bootstrap/Breadcrumb";
+import { getTelecommunicationMenusBySlug } from "../services/TelecommunicationFront";
 import { data } from "jquery";
+import { showAlert } from "../utils/showAlert";
 
 const Cpes = () => {
-  function Cpes({ data }) {
-    return (
-      <>
-        <div className="container ">
-          <div>
-            <h2 style={{ color: "#1D3557" }} className="pt-4">
-              {data.heading1}
-            </h2>
-          </div>
-          <div>
-            <p className="pt-4">{data.description1}</p>
-          </div>
-          <div>
-            <p>{data.description2}</p>
-          </div>
-        </div>
-      </>
-    );
-  }
+  const [cpe, setCpe] = useState({});
 
-  const obj = {
-    heading1: "CPEs",
-    description1:
-      "As a complement to Multitel's oer of services, we provide CPE's (Routers, Switches and other equipment) on a sale or rental basis and we are responsible for their installation and configuration.",
-    description2:
-      "We can extend the scope of our intervention, as an OMG - Operation, Maintenance and Management service , adapted to the Client's needs. Thus, companies or organizations that do not have aqualified technical team or specific know-how to carry out these activities can focus exclusively on their business.",
+  const handleAllMessage = async () => {
+    const data = { slug: "cpes-1" };
+    try {
+      const resp = await getTelecommunicationMenusBySlug(data);
+      console.log(resp);
+      setCpe(resp && resp.data.data);
+      // console.log("newsreq", resp);
+    } catch (error) {
+      showAlert("Something went wrong", "error");
+    }
   };
+
+  useEffect(() => {
+    handleAllMessage();
+  }, []);
+
+ 
   return (
     <>
       <LandingPage>
@@ -49,7 +43,18 @@ const Cpes = () => {
               </Breadcrumb>
             </div>
           </div>
-          <Cpes data={obj} />
+
+          <div className="container">
+          <div>
+            <h2 style={{ color: "#1D3557" }} className="pt-4">
+              {cpe.name}
+            </h2>
+          </div>
+          <div>
+            <p className="pt-2">{cpe.description}</p>
+          </div>
+          </div>
+        
         </div>
       </LandingPage>
     </>
